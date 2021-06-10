5177bcf237b457dc6b06582075d42b8c326f08b6
piotr.warsza <piotr.warsza@kantar.com>
2021-06-10 13:54:37 +0200

    Add project files.

---
+++ testGLS_Peter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testGLS_Peter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 4!");
+            Console.WriteLine("Temperature on {0:D} is {1}°C.", DateTime.Today, 23.4)
+        }
+    }
+}
